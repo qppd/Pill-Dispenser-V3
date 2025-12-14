c0f33bd87092ab06fdde61ce1570002d12c35915
Quezon Province Programmer, Developers <quezon.province.pd@gmail.com>
2025-12-14 18:04:26 +0800

    Add PillDispenserV3 with WiFiManager setup

---
+++ source/esp32/PillDispenserV3/PillDispenserV3.ino
@@ -0,0 +1,13 @@
+#include "WIFI_CONFIG.h"
+
+void setup() {
+ 
+    // put your setup code here, to run once:
+    Serial.begin(115200);
+    setupWiFiManager();
+
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:   
+}
